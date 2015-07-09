c43e925bbfd1fa3d8bfb744581ece984fad4d122
kndavies <kdavies@wccnet.edu>
2015-07-09 08:03:36 -0400

    Added blank Reader and Writer class.  Added test folder with one Test file

---
+++ src/HTMLProject/Reader.java
@@ -0,0 +1,4 @@
+package HTMLProject;
+public class Reader {
+
+}
---
+++ test/test/Test.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Kim
+ *
+ */
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
